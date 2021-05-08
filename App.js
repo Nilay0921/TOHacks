a08363ee5c8efdccd5f61d67a68da48dbde90977
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-05-08 13:59:47 -0400

    Firebase imports and whatnot :D

--- App.js
+++ App.js
@@ -1,6 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import firebase from "firebase";
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCcKFBHuyG-vpL9FvzBCyN7Gtj0fwDCoSQ",
+  authDomain: "tohacks-ff343.firebaseapp.com",
+  projectId: "tohacks-ff343",
+  storageBucket: "tohacks-ff343.appspot.com",
+  messagingSenderId: "381681073929",
+  appId: "1:381681073929:web:663178803d422c9dcb1dd8",
+  measurementId: "G-6GXD9VSYZS"
+};
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+// const getUsername = () => {
+//   var toReturn = null;
+//   firebase.firestore().collection("Users").doc("gkWaMtCQ2CTuSBBoM3jX").get().then((res) => {
+
+
+//     console.log("HEREEE2: ", res.data().username);
+//     toReturn = res.data().username;
+    
+
+//   })
+
+  
+//   return toReturn;
+// }
+
+
 
 export default function App() {
   return (
